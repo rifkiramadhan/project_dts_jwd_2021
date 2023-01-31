ca5e0b5270b9d042748348ccb3d250d3c310d371
Rifki Ramadhan <riifkiramadhan2@gmail.com>
2023-01-31 15:18:53 +0700

    rehosting project

--- Pertemuan 15/psb_dts/admin/data-peserta.php
+++ Pertemuan 15/psb_dts/admin/data-peserta.php
@@ -40,8 +40,7 @@
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.5.0/font/bootstrap-icons.css">
 
     <!-- Bootstrap CSS -->
-    <link href="../css/bootstrap.min.css" rel="stylesheet" 
-    integrity="sha384-KyZXEAg3QhqLMpG8r+8fhAXLRk2vvoC2f3B09zVXn8CA5QIVfZOJ3BCsw2P0p/We" crossorigin="anonymous">
+    <link rel="stylesheet" href="../css/bootstrap.min.css" >
 
     
 </head>
